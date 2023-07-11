ddbdeb518e7129540733af3f5fb72deacd180aa9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-12 01:53:02 +0600

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat possimus architecto laborum excepturi animi aspernatur totam adipisci, ad fuga explicabo placeat qui beatae accusamus? Fuga quibusdam reprehenderit ullam quam soluta?</h2>
+    </div>
+  );
+};
 
-export default App
+export default App;
